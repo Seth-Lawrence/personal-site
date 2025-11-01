8d1fbdfc99e88e8aec58f314279cc5f7e930fb5b
Seth Hobson <seth.hobson.dev@gmail.com>
2025-11-01 01:42:34 -0400

    added language card

---
+++ src/components/LanguageCard.tsx
@@ -0,0 +1,32 @@
+// import '../styles/Languages.scss';
+
+/** renders language icons
+ *
+ * props: none
+ *
+ * state: none
+ *
+ * App => Skills => Languages
+ */
+
+const LanguageCard = () => {
+    return (
+        <section className='Languages-container'>
+            <span className='Languages-title'>
+                Languages:
+            </span>
+            <div className='Languages'>
+                <i className="devicon-typescript-plain colored"></i>
+                <i className="devicon-javascript-plain colored"></i>
+                <i className="devicon-python-plain-wordmark colored"></i>
+            </div>
+            <div className='Languages'>
+                <i className="devicon-html5-plain-wordmark colored"></i>
+                <i className="devicon-css3-plain-wordmark colored"></i>
+                <i className="devicon-postgresql-plain-wordmark colored"></i>
+            </div>
+        </section>
+    );
+}
+
+export default LanguageCard;
