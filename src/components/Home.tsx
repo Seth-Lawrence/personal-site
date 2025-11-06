73d4ea00d6baa3acaef0001ca957bf9c837e5e22
Seth Hobson <seth.hobson.dev@gmail.com>
2025-11-05 20:51:12 -0500

    removed some commented-out code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -21,7 +21,7 @@ const Home = () => {
         <ProfilePicture />
         <h1>Hi, I'm Seth</h1>
         <h2>I'm a Software Developer with a background in Analytics </h2>
-        {/* <RecentProjects /> */}
+        
       </div>
 
   );
