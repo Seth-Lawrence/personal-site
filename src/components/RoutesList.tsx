51ff5bcd4938563d3d0a03483e7e3d8fc69f06ae
Seth Hobson <seth.hobson.dev@gmail.com>
2025-11-05 21:13:41 -0500

    removed skills from its own route and put on landing

--- src/components/RoutesList.tsx
+++ src/components/RoutesList.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import Skills from './Skills';
+// import Skills from './Skills';
 import Projects from './Projects';
 import About from './About';
 import Home from './Home';
@@ -8,7 +8,7 @@ const RoutesList = () => {
 
   return (
     <Routes>
-      <Route path='/skills' element={<Skills />} />
+      {/* <Route path='/skills' element={<Skills />} /> */}
       <Route path='/projects' element={<Projects />} />
       <Route path='/about' element={<About />} />
       <Route path='/home' element={<Home />} />
